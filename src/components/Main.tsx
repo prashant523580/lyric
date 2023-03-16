aa38bdeaf3ea81336990cf5338b9594f56736590
prashant523580 <prashant.thapa62758@gmail.com>
2023-03-17 00:22:00 +0545

    test

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,5 @@
-import { getAllSongLists } from '@/redux/actions/songlist.action';
-import { RootState,AppDispatch } from '@/redux/store';
+import { getAllSongLists } from 'redux/actions/songlist.action';
+import { RootState,AppDispatch } from 'redux/store';
 // import { AppDispatch } from '@/redux/store';
 import styles from "../styles/Home.module.css"
 import React, { useEffect } from 'react'
